5b45204380d9faac0a89c170a23100c4ca12991f
dagimdirba383-bit <dagimdirba383@gmail.com>
2026-05-27 20:01:06 +0300

    Update Hospital.java

--- Hospital.java
+++ Hospital.java
@@ -1,1 +1,19 @@
+public class hospital {
+  
+  private String hospitalName;
+  
+  public Hospital(String hospitalName) {
+    this.hospitalName=hospitalName;
+  }
 
+  public void showHospitalName() {
+    System.out.println("Hospital Name: " + hospitalname);
+  }
+  
+  public void openHospital() {
+    System.out.println(Hospital is now open.");
+  }
+    public void closeHospital() {
+        System.out.println("Hospital is now closed.");
+      }
+  }
