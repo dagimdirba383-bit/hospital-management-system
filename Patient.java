4f3345a670b8e77a86e1e9511147c060d925ec6a
dagimdirba383-bit <dagimdirba383@gmail.com>
2026-05-27 21:47:51 +0300

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,1 +1,29 @@
+public class Patient extends Person {
 
+  private String disease;
+
+  public Patient (String name, int age, String disease) {
+    super(name, age);
+    this.disease = disease;
+  }
+
+  public String getDisease() {
+    return disease;
+  }
+
+  public void setDisease(String disease) {
+    this.disease = disease;
+  }
+
+  public void displayRole() {
+    System.out.println("I am a Patient.");
+  }
+
+  public void takeMedicine() {
+    System.out.println("Patient is taking medicine.");
+  }
+
+  public void paybill() {
+    System.out.println("Patient paid the hospital bill");
+  }
+}
