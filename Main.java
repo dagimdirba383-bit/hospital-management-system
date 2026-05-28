edfb9ce88d068371cde60076591601f60827ceae
dagimdirba383-bit <dagimdirba383@gmail.com>
2026-05-29 02:31:17 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,36 @@
+public class Main {
 
+  public static void main(String[] args) {
+
+    Hospital hospital = new Hospital("City Hospital");
+    Doctor doctor = new Doctor(
+      "jhon",
+      45,
+      "Cardiology"
+      );
+
+    Patient patient = new Patient(
+      "Mike",
+      30,
+      "Fever"
+      );
+
+    hospital.showHospitalName();
+
+    doctor.displayRole();
+    patient.displayRole();
+
+    System.out.println("Doctor Name: " + doctor.getName());
+    System.out.println("Patient Name: " + patient.getName());
+
+    doctor.treatPatient();
+    doctor.prescribeMedicine();
+
+    patient.takeMedicine();
+    patient.payBill();
+
+    hospital.openHospital();
+  }
+}
+                                
+                                  
