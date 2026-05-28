3983336c3ee1e97ec45cc5a8d0d8535746793f64
dagimdirba383-bit <dagimdirba383@gmail.com>
2026-05-28 15:26:35 +0300

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,1 +1,29 @@
+public class Doctor extends Person {
 
+  private String specialization;
+
+  public Doctor(String name, int age, String specialization) {
+    super(name, age);
+    this.specialization = specialization;
+  }
+
+  public String getSpecialization() {
+    return specialization;
+  }
+
+  public void setSpecialization(String specialization) {
+    this.specialization = specialization;
+  }
+
+  public void displayRole() {
+    System.out.println("I am a Doctor.");
+  }
+
+  public void treatPatient() {
+    System.out.println("Doctor is treating a patient.");
+  }
+
+  public void prescribeMedicine() {
+    System.out.println("Medicine prescribed.");
+  }
+}
